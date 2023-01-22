0ef95eb61905710b0e1f99f1cf77eccd78e81328
AndrewDobriy <an.dobriy@mail.ru>
2023-01-22 14:34:55 +0300

    cart total fix

--- store.js
+++ store.js
@@ -5,5 +5,19 @@ for (let i = 0; i < removeCartItemsBtn.length; i++) {
   button.addEventListener('click', (e) => {
     let buttonClicked = e.target;
     buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
   });
+}
+
+function updateCartTotal() {
+  const cartItemContainer = document.getElementsByClassName('cart-items')[0];
+  const cartRows = cartItemContainer.getElementsByClassName('cart-row');
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i];
+    let priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    let quantityElement = cartRow.getElementsByClassName(
+      'cart-quantity-input'
+    )[0];
+    console.log(priceElement, quantityElement);
+  }
 }
